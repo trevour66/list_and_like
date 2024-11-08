5a0a2f9c52039239bf958720660cfed332e5bee8
Iniubong Peter <peteriniubong@gmail.com>
2024-11-08 21:35:18 +0100

    Handling Auth failure

--- resources/js/Pages/Profile/Edit.vue
+++ resources/js/Pages/Profile/Edit.vue
@@ -41,10 +41,10 @@ onMounted(() => {
 		<template #content>
 			<div class="py-12">
 				<div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-					<div class="p-4 sm:p-8" v-if="AuthError">
+					<div v-if="AuthError">
 						<div
 							id="alert-auth-error"
-							class="flex items-center p-4 mb-4 text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400"
+							class="flex items-center p-4 mb-4 text-red-800 rounded-lg bg-red-50"
 							role="alert"
 						>
 							<svg
@@ -65,7 +65,7 @@ onMounted(() => {
 							</div>
 							<button
 								type="button"
-								class="ms-auto -mx-1.5 -my-1.5 bg-blue-50 text-blue-500 rounded-lg focus:ring-2 focus:ring-blue-400 p-1.5 hover:bg-blue-200 inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800 dark:text-blue-400 dark:hover:bg-gray-700"
+								class="ms-auto -mx-1.5 -my-1.5 bg-red-50 text-red-500 rounded-lg focus:ring-2 focus:ring-red-400 p-1.5 hover:bg-red-200 inline-flex items-center justify-center h-8 w-8"
 								data-dismiss-target="#alert-auth-error"
 								aria-label="Close"
 							>
