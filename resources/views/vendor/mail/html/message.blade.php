0dc1944d7bb9de75d97bf0bbfbbfa3be1101b858
Iniubong Peter <peteriniubong@gmail.com>
2024-11-27 07:47:11 +0100

    new changes

--- resources/views/vendor/mail/html/message.blade.php
+++ resources/views/vendor/mail/html/message.blade.php
@@ -1,27 +1,28 @@
 <x-mail::layout>
-{{-- Header --}}
-<x-slot:header>
-<x-mail::header :url="config('app.url')">
-{{ config('app.name') }}
-</x-mail::header>
-</x-slot:header>
+    {{-- Header --}}
+    <x-slot:header>
+        <x-mail::header :url="config('app.url')">
+            <img src="https://listandlike.com/images/Logo.png" :alt="config('app.name')" class=" logo">
+            {{ config('app.name') }}
+        </x-mail::header>
+    </x-slot:header>
 
-{{-- Body --}}
-{{ $slot }}
+    {{-- Body --}}
+    {{ $slot }}
 
-{{-- Subcopy --}}
-@isset($subcopy)
-<x-slot:subcopy>
-<x-mail::subcopy>
-{{ $subcopy }}
-</x-mail::subcopy>
-</x-slot:subcopy>
-@endisset
+    {{-- Subcopy --}}
+    @isset($subcopy)
+    <x-slot:subcopy>
+        <x-mail::subcopy>
+            {{ $subcopy }}
+        </x-mail::subcopy>
+    </x-slot:subcopy>
+    @endisset
 
-{{-- Footer --}}
-<x-slot:footer>
-<x-mail::footer>
-© {{ date('Y') }} {{ config('app.name') }}. @lang('All rights reserved.')
-</x-mail::footer>
-</x-slot:footer>
+    {{-- Footer --}}
+    <x-slot:footer>
+        <x-mail::footer>
+            © {{ date('Y') }} {{ config('app.name') }}. @lang('All rights reserved.')
+        </x-mail::footer>
+    </x-slot:footer>
 </x-mail::layout>
