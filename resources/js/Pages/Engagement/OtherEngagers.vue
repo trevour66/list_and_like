d151f4609f95d00494651f97361b01c9f007ce82
Iniubong Peter <peteriniubong@gmail.com>
2024-12-04 00:07:12 +0100

    UI changes

--- resources/js/Pages/Engagement/OtherEngagers.vue
+++ resources/js/Pages/Engagement/OtherEngagers.vue
@@ -112,66 +112,80 @@ onMounted(async () => {
 
 <template>
 	<div class="relative shadow-md sm:rounded-lg p-2 bg-white">
-		<div class="max-w-full overflow-x-auto">
-			<div class="max-h-[600px] overflow-y-auto" id="other_engagers">
-				<table
-					class="table-auto w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400"
-				>
-					<thead class="sticky top-0 bg-gray-100">
-						<tr>
-							<th
-								v-for="(header, index) in tableHeaders"
-								scope="col"
-								class="font-bold px-6 py-3"
-								:class="{
-									'bg-gray-50': index % 2 !== 1,
-									'bg-white': index % 2 === 1,
-								}"
-							>
-								{{ header }}
-							</th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr v-for="row in tableRows" class="border-b border-gray-200">
-							<td
-								scope="row"
-								class="px-6 py-4 font-medium text-gray-600 whitespace-nowrap bg-gray-50"
-							>
-								<div
-									class="inline-flex flex-row gap-x-4 items-center justify-center"
-								>
-									<span>
-										{{ row?.ig_handle ?? "" }}
-									</span>
-									<span>
-										<div
-											@click="
-												passthrough_goToIGProfilePosts(row?.ig_handle ?? '')
-											"
-											class="p-1 rounded-lg border-2 shadow-md hover:shadow-sm hover:cursor-pointer"
-										>
-											<ExpandIcon />
-										</div>
-									</span>
-								</div>
-							</td>
-
-							<td class="px-6 py-4 bg-white">{{ row?.postCount ?? "" }}</td>
-						</tr>
-						<tr v-if="(tableRows ?? []).length == 0">
-							<div class="flex items-center justify-center w-full h-20">
-								No data
-							</div>
-						</tr>
-						<tr v-if="Loading">
-							<div class="flex items-center justify-center w-full h-20">
-								<InfinityScrollLoader />
-							</div>
-						</tr>
-					</tbody>
-				</table>
+		<template v-if="(tableRows ?? []).length == 0">
+			<div
+				class="flex items-center justify-center w-full h-full bg-gray-50 mt-10"
+			>
+				<div>
+					<p class="text-md font-normal text-gray-500">
+						You does not have an item in this list
+					</p>
+				</div>
 			</div>
-		</div>
+		</template>
+
+		<template v-else>
+			<div class="max-w-full overflow-x-auto">
+				<div class="max-h-[600px] overflow-y-auto" id="other_engagers">
+					<table
+						class="table-auto w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400"
+					>
+						<thead class="sticky top-0 bg-gray-100">
+							<tr>
+								<th
+									v-for="(header, index) in tableHeaders"
+									scope="col"
+									class="font-bold px-6 py-3"
+									:class="{
+										'bg-gray-50': index % 2 !== 1,
+										'bg-white': index % 2 === 1,
+									}"
+								>
+									{{ header }}
+								</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr v-for="row in tableRows" class="border-b border-gray-200">
+								<td
+									scope="row"
+									class="px-6 py-4 font-medium text-gray-600 whitespace-nowrap bg-gray-50"
+								>
+									<div
+										class="inline-flex flex-row gap-x-4 items-center justify-center"
+									>
+										<span>
+											{{ row?.ig_handle ?? "" }}
+										</span>
+										<span>
+											<div
+												@click="
+													passthrough_goToIGProfilePosts(row?.ig_handle ?? '')
+												"
+												class="p-1 rounded-lg border-2 shadow-md hover:shadow-sm hover:cursor-pointer"
+											>
+												<ExpandIcon />
+											</div>
+										</span>
+									</div>
+								</td>
+
+								<td class="px-6 py-4 bg-white">{{ row?.postCount ?? "" }}</td>
+							</tr>
+							<tr v-if="(tableRows ?? []).length == 0">
+								<div class="flex items-center justify-center w-full h-20">
+									No data
+								</div>
+							</tr>
+							<tr v-if="Loading">
+								<div class="flex items-center justify-center w-full h-20">
+									<InfinityScrollLoader />
+								</div>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</template>
 	</div>
 </template>
