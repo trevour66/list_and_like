24cd96b78cdd7016beae766379d674e043a2d11c
Iniubong Peter <peteriniubong@gmail.com>
2025-01-20 16:21:32 +0100

    fix: mulitple ready mails

--- app/Notifications/SuccessfulDataFetch_noNewProfile.php
+++ app/Notifications/SuccessfulDataFetch_noNewProfile.php
@@ -44,7 +44,7 @@ class SuccessfulDataFetch_noNewProfile extends Notification
 
         return (new MailMessage)
             // ->subject("Data Sync Completed Successfully - {$this->IGAccessCodes->IG_USERNAME}")
-            ->subject("Your List & Like Account is Ready!")
+            ->subject("Data Sync Completed - No Eligible Activity Detected in Your Recent Instagram Data")
             ->greeting("Hello")
             ->line('Your data syncing process is complete! However, the system did not pickup anynew data.')
             ->line("Currently, our system analyzes data from the past 3 months. It seems there were either no new posts from you during this period, or the system couldn't identify any Instagram profiles that have commented on at least five of your posts. If you believe this is incorrect, please don't hesitate to contact support for assistance.")
